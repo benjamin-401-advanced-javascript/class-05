1342871d9604ae9dee1f61d9cb23c5814ae1ee0d
Benjamin Clark <benisin@gmail.com>
2019-08-18 10:32:36 -0700

    init

---
+++ models-singular/categories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const categoriesModel = require('./categories-schema');
+
+class Categories {
+
+  constructor() {
+  }
+
+  get(_id) {
+    // Call the appropriate mongoose method to get
+    // one or more records
+    // If 1, return it as a plain object
+    // If 2, return it as an object like this:
+    // { count: ##, results: [{}, {}] }
+  }
+
+  create(record) {
+    // Call the appropriate mongoose method to create a new record
+  }
+
+  update(_id, record) {
+    // Call the appropriate mongoose method to update a record
+  }
+
+  delete(_id) {
+    // Call the appropriate mongoose method to delete a record
+  }
+
+}
+
+module.exports = Categories;
